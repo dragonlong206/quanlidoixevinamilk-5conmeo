fbd0797a54e4682d09cb86350aa1b1084d545556
hoangdang1189 <hoangdang1189@dcabb89d-9eb2-3196-d1d8-2bcd523145b7>
2010-06-05 08:35:45 +0000

    Hoang finish chuyen BUS, DAO, DTO: project Console => Class Library

---
+++ trunk/SourceCode/QuanLyDoiXeVinamilk/BUS/HangXeBUS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BUS
+{
+    public class HangXeBUS
+    {
+        //Tam thoi dung ham nay: chinh sua truy xuat CSDL sau.
+        public static int GetMaHangXe(String strTenHangXe)
+        {
+            switch(strTenHangXe)
+            {
+                case "TOYOTA":
+                    return 1;
+                case "HONDA":
+                    return 2;
+                case "MERCIDES":
+                    return 3;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
---
+++ trunk/SourceCode/QuanLyDoiXeVinamilk/DAO/XeDAO.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+
+
+namespace DAO
+{
+    public class XeDAO
+    {
+        private static string strLoaiKetNoi = "Provider = Microsoft.Jet.OLEDB.4.0;";
+        private static string strCSDL = "..\\DataBase\\Db_QuanLyDoiXeVinamilk.accdb";
+        private static string strChuoiKetNoi = strLoaiKetNoi + strCSDL;
+
+        public static OleDbConnection MoKetNoi()
+        {
+            OleDbConnection contKetNoi;
+
+            try
+            {
+                contKetNoi = new OleDbConnection(strChuoiKetNoi);
+            }
+            catch (System.Exception ex)
+            {
+                //MessageBox.Show("Loi ket noi CSDL" + strChuoiKetNoi + ex.Message);
+                return null;
+            }
+
+            return contKetNoi;
+        }
+
+        public static DataTable Doc(String strChuoiLoc)
+        {
+            DataTable tblBang = new DataTable();
+
+            try
+            {
+                OleDbConnection contKetNoi = MoKetNoi();
+                OleDbDataAdapter adaptBoThichUng = new OleDbDataAdapter(strChuoiLoc, contKetNoi);
+                adaptBoThichUng.Fill(tblBang);
+            }
+            catch (System.Exception ex)
+            {
+                //MessageBox.Show("Loi ket noi " + strChuoiLoc + ex.Message);
+                return null;
+            }            
+
+            return tblBang;
+        }
+
+        public static bool Ghi(DataTable tblBang, String strTenBang)
+        {
+            bool blnKetQua = true;
+            String strChuoiLoc = "Select * From " + strTenBang;
+
+            try
+            {
+                OleDbConnection contKetNoi = MoKetNoi();
+                OleDbDataAdapter adaptBoThichUng = new OleDbDataAdapter(strChuoiLoc, contKetNoi);
+                OleDbCommandBuilder comBoPhatSinh = new OleDbCommandBuilder(adaptBoThichUng);
+                adaptBoThichUng.Update(tblBang);
+            }
+            catch (System.Exception ex)
+            {
+                //MessageBox.Show("Loi ghi CSDL " + strChuoiLoc + ex.Message);
+                blnKetQua = false;
+            }
+            return blnKetQua;
+        }
+    }
+}
