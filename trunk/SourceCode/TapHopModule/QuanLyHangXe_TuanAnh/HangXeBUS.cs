df54f2a40891c466fe65dec3d1fba68240a691f5
hoangdang1189 <hoangdang1189@dcabb89d-9eb2-3196-d1d8-2bcd523145b7>
2010-06-07 11:45:26 +0000

    Tuan Anh bo sung chuc nang QuanLyHangXe (70%). - Con chuc nang cap nhat thong tin HangXe

---
+++ trunk/SourceCode/TapHopModule/QuanLyHangXe_TuanAnh/HangXeBUS.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DTO;
+using DAO;
+using System.Data.OleDb;
+using System.Data;
+
+namespace BUS
+{
+    public class HangXeBUS
+    {
+        //Tam thoi dung ham nay: chinh sua truy xuat CSDL sau.
+          public static int GetMaHangXe(String strTenHangXe)
+          {
+              switch(strTenHangXe)
+              {
+                  case "TOYOTA":
+                      return 1;
+                  case "HONDA":
+                      return 2;
+                  case "MERCIDES":
+                      return 3;
+                  default:
+                      return -1;
+              }
+          }
+        #region 1.Insert
+        public static Boolean ThemHangXeBus(HangXeDTO HangXe)
+        {
+            Boolean blnDaThemDuoc = false;
+            try
+            {
+                blnDaThemDuoc = HangXeDAO.ThemHangXeDAO(HangXe);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return blnDaThemDuoc;
+
+        }
+        #endregion
+      
+
+        #region 2.Delete
+        public static Boolean XoaHangXeBUS(HangXeDTO HangXe)
+        {
+            Boolean blnDaXoaDuoc = false;
+            try
+            {
+                blnDaXoaDuoc = HangXeDAO.XoaHangXeDAO(HangXe);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            return blnDaXoaDuoc;
+        }
+        #endregion
+        #region 3.Update
+        public static bool CapNhatHangXe(HangXeDTO  hx)
+        {
+            try
+            {
+                if (HangXeDAO.KiemTraMaHangXe(hx.MaHang))
+                {
+                    return HangXeDAO.CapNhatHangXeDAO(hx);
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        #endregion
+        #region 4.Select
+        public static List<HangXeDTO> LayDanhSachTatCaCacHangXe()
+        {
+            return HangXeDAO.LayDanhSachTatCaCacHangXe();
+        }
+        public static HangXeDTO LayDanhSachHangXeTheMaHangXe(int MaHangXe)
+        {
+            if (HangXeDAO.KiemTraMaHangXe(MaHangXe))
+            {
+                return null;
+            }
+            return HangXeDAO.LayHangXeTheoMaHang(MaHangXe);
+        }
+
+        #endregion
+
+    }
+}
---
+++ trunk/SourceCode/TapHopModule/QuanLyHangXe_TuanAnh/HangXeDAO.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DTO;
+using System.Data.OleDb;
+using System.Data;
+
+namespace DAO
+{
+   public class HangXeDAO
+    {
+        #region 1.Insert
+       public static Boolean ThemHangXeDAO(HangXeDTO HangXe)
+       {
+           Boolean blnDaThemDuoc = false;
+           try
+           {
+               List<OleDbParameter> arrParams = new List<OleDbParameter>();
+               arrParams.Add(new OleDbParameter("@MaHang",HangXe.MaHang));
+               arrParams.Add(new OleDbParameter("@TenHang",HangXe.TenHang));
+               String strCommand = @"Insert into HANG_XE (MaHang,TenHang)values(?,?)";
+               int nRecord = SqlDataAccessHelper.ExcuteNonQuery(strCommand, arrParams);
+               if (nRecord == 1)
+               {
+                   blnDaThemDuoc = true;
+
+               }
+           }
+           catch (Exception ex)
+           {
+               throw ex;
+           }
+           return blnDaThemDuoc;
+       }
+        #endregion
+        #region 2.Delete
+       public static Boolean XoaHangXeDAO(HangXeDTO hx)
+       {
+           Boolean blnKetQua = false;
+
+           try
+           {
+               List<OleDbParameter> arrParams = new List<OleDbParameter>();
+               arrParams.Add(new OleDbParameter("@MaHang", hx.MaHang));
+               String strCommand = @"Delete from HANG_XE
+                                      Where MaHang = ?";
+
+               int nRecord = SqlDataAccessHelper.ExcuteNonQuery(strCommand, arrParams);
+               if (nRecord == 1)
+               {
+                   blnKetQua = true;
+               }
+           }
+           catch (Exception ex)
+           {
+               throw ex;
+           }
+
+           return blnKetQua;
+       }
+        #endregion
+        #region 3.Update
+       public static bool CapNhatHangXeDAO(HangXeDTO hx)
+       {
+           bool result = false;
+           try
+           {
+               // Create List Sql Parameter
+               List<OleDbParameter> arrParams = new List<OleDbParameter>();
+               arrParams.Add(new OleDbParameter("@MaHang", hx.MaHang));
+               arrParams.Add(new OleDbParameter("@TenHang", hx.TenHang));
+              
+              //chi cap nhat lai ten hang xe 
+               String strCommand = @"Update HANG_XE set TenHang = ? where MaHang = @MaHang";
+               int nRecord = SqlDataAccessHelper.ExcuteNonQuery(strCommand, arrParams);
+               if (nRecord == 1)
+               {
+                   result = true;
+
+               }
+           }
+           catch (Exception ex)
+           {
+               throw ex;
+           }
+           return result;
+       }
+        #endregion
+        #region 4.Select 
+       public static List<HangXeDTO> LayDanhSachTatCaCacHangXe()
+       {
+           List<HangXeDTO> dsHangXe = new List<HangXeDTO>();
+
+           String strCommand = "Select * from Hang_Xe";
+           DataTable table = SqlDataAccessHelper.ExcuteQuery(strCommand, null);
+
+           foreach (DataRow row in table.Rows)
+           {
+               HangXeDTO hangxe = new HangXeDTO();
+               hangxe.MaHang = int.Parse(row["MaHang"].ToString());
+               hangxe.TenHang = row["TenHang"].ToString();
+               dsHangXe.Add(hangxe);
+           }
+
+           return dsHangXe;
+       }
+
+       public static HangXeDTO LayHangXeTheoMaHang(int MaHang)
+       {
+           HangXeDTO hangxe = null;
+
+           String strCommand = "Select * from Hang_Xe where MaHang = ?";
+           OleDbParameter param = new OleDbParameter("@MaHang", MaHang);
+           List<OleDbParameter> arrParams = new List<OleDbParameter>();
+           arrParams.Add(param);
+
+           DataTable table = SqlDataAccessHelper.ExcuteQuery(strCommand, arrParams);
+
+           if (table.Rows.Count > 0)
+           {
+               hangxe = new HangXeDTO();
+               hangxe.MaHang = int.Parse(table.Rows[0]["MaHang"].ToString());
+               hangxe.TenHang = table.Rows[0]["TenHang"].ToString();
+             
+           }
+
+           return hangxe;
+       }
+       public static bool KiemTraMaHangXe(int MaHangXe)
+       {
+           try
+           {
+               HangXeDTO hangxe = HangXeDAO.LayHangXeTheoMaHang(MaHangXe);
+
+               if (hangxe != null)
+               {
+                   return true;
+               }
+           }
+           catch (Exception ex)
+           {
+               throw ex;
+           }
+
+           return false;
+       }
+
+        #endregion
+    }
+}
