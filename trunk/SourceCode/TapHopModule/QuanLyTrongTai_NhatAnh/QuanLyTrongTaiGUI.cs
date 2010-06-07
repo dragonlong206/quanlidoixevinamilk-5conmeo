c0e54e7db61605f3f032372435ea33ee6d3bb4e0
n.anhdonguyen <n.anhdonguyen@dcabb89d-9eb2-3196-d1d8-2bcd523145b7>
2010-06-07 14:20:58 +0000

    cap nhat trong tai NA chua lam, khi them 1 trong tai moi thi list ben trai chua hien luon trong tai vua them!

---
+++ trunk/SourceCode/TapHopModule/QuanLyTrongTai_NhatAnh/QuanLyTrongTaiGUI.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BUS;
+using DTO;
+
+namespace GUI
+{
+    public partial class frm_QuanLyTrongTai : Form
+    {
+        public frm_QuanLyTrongTai()
+        {
+            InitializeComponent();
+        }
+        #region Them Trong Tai
+        private void btnThemTrongTai_Click(object sender, EventArgs e)
+        {
+            DTO.TrongTaiDTO aTrongTai = NhapTrongTai();
+            if (aTrongTai == null)
+                return; //khong lam gi ca.    
+            try
+            {
+                String strThongBao = "Bi loi ghi du lieu: vui long kiem tra du lieu nhap";
+                if (BUS.TrongTaiBUS.ThemTrongTai(aTrongTai))
+                {
+                    strThongBao = "Them trong tai moi thanh cong";
+                }
+                MessageBox.Show(strThongBao);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Exception:" + ex.Message);
+            }
+
+        }
+
+        private DTO.TrongTaiDTO NhapTrongTai()
+        {
+            DTO.TrongTaiDTO aTrongTai = new DTO.TrongTaiDTO();
+            if (!KiemTraNhap())  //Cac dong thong bao cu the duoc thuc hien trong KiemTraNhap()                          
+                return null;
+
+            try
+            {
+                aTrongTai.MaTrongTai = int.Parse(txt_MaTrongTai.Text);
+                aTrongTai.GiaTri = int.Parse(txt_GiaTri.Text);
+                aTrongTai.DonVi = txt_DonVi.Text;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Exception:" + ex.Message);
+                return null;
+            }
+            return aTrongTai;
+        }
+
+        private bool KiemTraNhap()
+        {
+            bool blnKetQua = true;
+
+            blnKetQua &= !String.IsNullOrEmpty(txt_MaTrongTai.Text);
+            blnKetQua &= !String.IsNullOrEmpty(txt_GiaTri.Text);
+            if (!blnKetQua)
+            {
+                MessageBox.Show("Du lieu nhap vao khong day du\r\nVui long kiem tra lai");
+                return blnKetQua;
+            }
+            return blnKetQua;
+        }
+        #endregion
+
+        #region Xoa Trong Tai
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (lsv_DanhSachHienCo.CheckedItems.Count > 0)
+            {
+                try
+                {
+                    foreach (ListViewItem itemTrongTai in lsv_DanhSachHienCo.CheckedItems)
+                    {
+                        DTO.TrongTaiDTO aTrongTai = (DTO.TrongTaiDTO)itemTrongTai.Tag;
+                        if (BUS.TrongTaiBUS.XoaTrongTai(aTrongTai))
+                        {
+                            lsv_DanhSachHienCo.Items.Remove(itemTrongTai);
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Exception: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Chua co trong tai nao duoc chon");
+            }
+        }
+        #endregion
+
+        #region Thoat
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        #region Load: Xuat thong tin TrongTai
+        private void frm_QuanLyTrongTai_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                List<DTO.TrongTaiDTO> lstTrongTai = TrongTaiBUS.DocDanhSachTrongTai();
+                XuatDanhSachTrongTai(lstTrongTai);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        private void XuatDanhSachTrongTai(List<DTO.TrongTaiDTO> lstTrongTai)
+        {
+            DTO.TrongTaiDTO aTrongTai = lstTrongTai[0];
+            if (aTrongTai != null)
+            {
+              //  XuatChiTietTrongTai(aTrongTai);
+            }
+
+            lsv_DanhSachHienCo.Items.Clear();
+           foreach (DTO.TrongTaiDTO aTrongTaiTemp in lstTrongTai)
+           {
+                lsv_DanhSachHienCo.Items.Add(TheHienListItem(aTrongTaiTemp));
+           }
+        }
+        private ListViewItem TheHienListItem(DTO.TrongTaiDTO aTrongTai)
+        {
+            ListViewItem itemKetQua = new ListViewItem(aTrongTai.MaTrongTai.ToString());
+            itemKetQua.SubItems.Add(aTrongTai.GiaTri.ToString());
+            itemKetQua.SubItems.Add(aTrongTai.DonVi.ToString());
+            itemKetQua.Tag = aTrongTai;
+
+            return itemKetQua;
+        }
+
+        private void lsv_DanhSachHienCo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lsv_DanhSachHienCo.SelectedItems.Count > 0)
+            {
+                ListViewItem itemTrongTai = lsv_DanhSachHienCo.SelectedItems[0];
+                DTO.TrongTaiDTO aTrongTai = (DTO.TrongTaiDTO)itemTrongTai.Tag;
+                XuatChiTietTrongTai(aTrongTai);
+            }
+        }
+        private void XuatChiTietTrongTai(DTO.TrongTaiDTO aTrongTai)
+        {
+            txt_MaTrongTai.Text = aTrongTai.MaTrongTai.ToString();
+            txt_GiaTri.Text = aTrongTai.GiaTri.ToString();
+            txt_DonVi.Text = aTrongTai.DonVi;
+        }
+
+        private void lbl_GiaTri_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        private void grb_TrongTaiHienCo_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Cap Nhat Trong Tai
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ trunk/SourceCode/TapHopModule/QuanLyTrongTai_NhatAnh/TrongTaiDAO.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+using DTO;
+
+namespace DAO
+{
+    public class TrongTaiDAO
+    {
+        #region select
+        public static List<TrongTaiDTO> DocDanhSachTrongTai()
+        {
+            List<TrongTaiDTO> dsTrongTai = new List<TrongTaiDTO>();
+
+            String strCommand = "Select * from TRONG_TAI";
+            DataTable table = SqlDataAccessHelper.ExcuteQuery(strCommand, null);
+
+            foreach (DataRow aDong in table.Rows)
+            {
+                dsTrongTai.Add(KhoiTao(aDong));
+            }
+
+            return dsTrongTai;
+        }
+        public static DTO.TrongTaiDTO KhoiTao(DataRow aDong)
+        {
+            DTO.TrongTaiDTO aTrongTai = new DTO.TrongTaiDTO();
+            aTrongTai.MaTrongTai = int.Parse(aDong["MaTrongTai"].ToString());
+            aTrongTai.GiaTri = int.Parse(aDong["GiaTri"].ToString());
+            aTrongTai.DonVi = aDong["DonVi"].ToString();
+            return aTrongTai;
+        }
+
+        public static TrongTaiDTO LayTrongTaiTheoMTT(int nMaTrongTai)
+        {
+            TrongTaiDTO trongtai = null;
+
+            String strCommand = "Select * from TRONG_TAI where MaTrongTai = ?";
+            OleDbParameter param = new OleDbParameter("@MaTrongTai", nMaTrongTai);
+            List<OleDbParameter> arrParams = new List<OleDbParameter>();
+            arrParams.Add(param);
+
+            DataTable table = SqlDataAccessHelper.ExcuteQuery(strCommand, arrParams);
+
+            if (table.Rows.Count > 0)
+            {
+                trongtai = new TrongTaiDTO();
+                trongtai.MaTrongTai = int.Parse(table.Rows[0]["MaTrongTai"].ToString());
+                trongtai.GiaTri = int.Parse(table.Rows[0]["GiaTri"].ToString());
+                trongtai.DonVi = table.Rows[0]["DonVi"].ToString();
+            }
+
+            return trongtai;
+        }
+
+        #endregion
+
+        #region delete
+        public static Boolean XoaTrongTai(DTO.TrongTaiDTO aTrongTai)
+        {
+            Boolean blnKetQua = false;
+
+            try
+            {
+                List<OleDbParameter> arrParams = new List<OleDbParameter>();
+                arrParams.Add(new OleDbParameter("@MaTrongTai", aTrongTai.MaTrongTai));
+                String strCommand = @"Delete from TRONG_TAI
+                                      Where MaTrongTai = ?";
+
+                int nRecord = SqlDataAccessHelper.ExcuteNonQuery(strCommand, arrParams);
+                if (nRecord == 1)
+                {
+                    blnKetQua = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return blnKetQua;
+        }
+        #endregion
+
+        #region insert
+        public static Boolean ThemTrongTai(TrongTaiDTO trongtai)
+        {
+            Boolean blnDaThemDuoc = false;
+
+            try
+            {
+                List<OleDbParameter> arrParams = new List<OleDbParameter>();
+                arrParams.Add(new OleDbParameter("@MaTrongTai", trongtai.MaTrongTai));
+                arrParams.Add(new OleDbParameter("@GiaTri", trongtai.GiaTri));
+                arrParams.Add(new OleDbParameter("@DonVi", trongtai.DonVi));
+
+                String strCommand = @"Insert into TRONG_TAI(MaTrongTai, GiaTri, DonVi)
+                                    values (?, ?, ?)";
+
+                int nRecord = SqlDataAccessHelper.ExcuteNonQuery(strCommand, arrParams);
+                if (nRecord == 1) // Số dòng insert được
+                {
+                    blnDaThemDuoc = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return blnDaThemDuoc;
+        }
+        #endregion
+
+    }
+}
