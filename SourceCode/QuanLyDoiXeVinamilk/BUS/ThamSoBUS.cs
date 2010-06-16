c1df65763b448f8f69366366145c0bec5ef7065d
lv.long.tn <lv.long.tn@dcabb89d-9eb2-3196-d1d8-2bcd523145b7>
2010-06-16 14:17:48 +0000

    Hoàn thành chức năng phân công xe tài xế. Lớp BUS

--- SourceCode/QuanLyDoiXeVinamilk/BUS/ThamSoBUS.cs
+++ SourceCode/QuanLyDoiXeVinamilk/BUS/ThamSoBUS.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using DAO;
 
 namespace BUS
 {
@@ -10,6 +12,22 @@ namespace BUS
         public static int GetSoNamToiDa()
         {
             return 10;
+        }
+
+        public static DataTable LayThamSo(String strTenThamSo)
+        {
+            DataTable KetQua = null;
+
+            try
+            {
+                KetQua = ThamSoDAO.LayThamSo(strTenThamSo);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return KetQua;
         }
     }
 }
