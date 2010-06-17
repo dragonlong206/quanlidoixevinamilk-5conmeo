f724030e13007b71efccad2df9b68e758862d17a
lv.long.tn <lv.long.tn@dcabb89d-9eb2-3196-d1d8-2bcd523145b7>
2010-06-17 18:28:12 +0000

    Nhập thông tin hóa đơn. Lớp BUS.

---
+++ trunk/SourceCode/QuanLyDoiXeVinamilk/BUS/ChiTietHoaDonBUS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DTO;
+using DAO;
+using System.Data;
+
+namespace BUS
+{
+    public class ChiTietHoaDonBUS
+    {
+        #region 1.Insert
+        public static bool ThemChiTiet(ChiTietHoaDonDTO aChiTiet)
+        {
+            bool blnDaThemDuoc = false;
+
+            try
+            {
+                int nSoDongThemDuoc = ChiTietHoaDonDAO.ThemChiTiet(aChiTiet);
+                if (nSoDongThemDuoc == 1)
+                {
+                    blnDaThemDuoc = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return blnDaThemDuoc;
+        }
+        #endregion
+    }
+}
