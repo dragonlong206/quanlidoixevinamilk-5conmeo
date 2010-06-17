c25bd5e069e1a206b00a795f7e8ca6b33ff77e3e
lv.long.tn <lv.long.tn@dcabb89d-9eb2-3196-d1d8-2bcd523145b7>
2010-06-17 18:30:12 +0000

    Nhập thông tin hóa đơn. Lớp DTO.

---
+++ trunk/SourceCode/QuanLyDoiXeVinamilk/DTO/ChiTietHoaDonDTO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO
+{
+    public class ChiTietHoaDonDTO
+    {
+        #region Thuộc tính
+        private int m_iMa;
+        private int m_iMaDiaDiem;
+        private int m_iMaHoaDon;
+        private int m_iMaMatHang;
+        private int m_iMaTrangThai;
+        private int m_nSoLuong;
+        #endregion
+
+        #region Phương thức truy xuất
+        public int Ma
+        {
+            get { return m_iMa; }
+            set { m_iMa = value; }
+        }
+
+        public int MaDiaDiem
+        {
+            get { return m_iMaDiaDiem; }
+            set { m_iMaDiaDiem = value; }
+        }
+
+        public int MaHoaDon
+        {
+            get { return m_iMaHoaDon; }
+            set { m_iMaHoaDon = value; }
+        }
+
+        public int MaMatHang
+        {
+            get { return m_iMaMatHang; }
+            set { m_iMaMatHang = value; }
+        }
+
+        public int MaTrangThai
+        {
+            get { return m_iMaTrangThai; }
+            set { m_iMaTrangThai = value; }
+        }
+
+        public int SoLuong
+        {
+            get { return m_nSoLuong; }
+            set { m_nSoLuong = value; }
+        }
+        #endregion
+
+        #region Phương thức thiết lập
+
+        #endregion
+    }
+}
---
+++ trunk/SourceCode/QuanLyDoiXeVinamilk/DTO/HoaDonDTO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO
+{
+    public class HoaDonDTO
+    {
+        #region Thuộc tính
+
+        private int m_iMa;
+        private DateTime m_NgayGiaoHang;
+        private DateTime m_NgayLap;
+        private String m_XeGiaoHang = String.Empty;
+
+        #endregion
+
+        #region Phương thức truy xuất
+        public int Ma
+        {
+            get { return m_iMa; }
+            set { m_iMa = value; }
+        }
+
+        public DateTime NgayGiaoHang
+        {
+            get { return m_NgayGiaoHang; }
+            set { m_NgayGiaoHang = value; }
+        }
+
+        public DateTime NgayLap
+        {
+            get { return m_NgayLap; }
+            set { m_NgayLap = value; }
+        }
+
+        public String XeGiaoHang
+        {
+            get { return m_XeGiaoHang; }
+            set { m_XeGiaoHang = value; }
+        }
+        #endregion
+    }
+}
