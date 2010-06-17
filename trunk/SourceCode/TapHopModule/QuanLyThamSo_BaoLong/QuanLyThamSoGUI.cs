f7ee9ca88395d36278cdd8a6214869692be40b28
horizontaldragon <horizontaldragon@dcabb89d-9eb2-3196-d1d8-2bcd523145b7>
2010-06-17 17:55:59 +0000

    Them chuc nang quan ly tham so

---
+++ trunk/SourceCode/TapHopModule/QuanLyThamSo_BaoLong/QuanLyThamSoGUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class QuanLyThamSoGUI : Form
+    {
+        public QuanLyThamSoGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void QuanLyThamSoGUI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
