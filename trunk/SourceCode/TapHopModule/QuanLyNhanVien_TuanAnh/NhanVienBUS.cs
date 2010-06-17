70f94809eeb7731eac09441fec8f98e1154754da
hoangdang1189 <hoangdang1189@dcabb89d-9eb2-3196-d1d8-2bcd523145b7>
2010-06-17 09:00:37 +0000

    Tuan Anh them chuc nang qlNhanVien (75%)

---
+++ trunk/SourceCode/TapHopModule/QuanLyNhanVien_TuanAnh/NhanVienBUS.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DTO;
+using DAO;
+using System.Data.OleDb;
+using System.Data;
+
+
+namespace BUS
+{
+    public class NhanVienBUS
+    {
+        //Tam thoi dung ham nay: chinh sua truy xuat CSDL sau.
+        public static int GetMaNhanVien(String strTenNhanVien)
+        {
+            switch(strTenNhanVien)
+            {
+                case "Bùi Kim Hoa":
+                    return 1;
+                case "Nguyễn Anh":
+                    return 2;
+                case "Nguyễn Khuyến":
+                    return 3;
+                default:
+                    return -1;
+            }
+        }
+
+        public static String GetTenNhanVien(int nMaNhanVien)
+        {
+            switch(nMaNhanVien)
+            {
+                case 1:
+                    return "Bùi Kim Hoa";
+                case 2:
+                    return "Nguyễn Anh";
+                case 3:
+                    return "Nguyễn Khuyến";
+                default:
+                    return "Erorr!";
+            }
+        }
+        public static Boolean ThemNhanVien(NhanVienDTO NhanVien)
+        {
+            Boolean blnKetQua = false;
+            try
+            {
+                blnKetQua = NhanVienDAO.ThemNhanVien(NhanVien);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return blnKetQua;
+
+        }
+        public static Boolean XoaNhanVien(NhanVienDTO NhanVien)
+        {
+            Boolean blnKetQua = false;
+            try
+            {
+                blnKetQua = NhanVienDAO.XoaNhanVien(NhanVien);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            return blnKetQua;
+        }
+        public static List<NhanVienDTO> DocDanhSachNhanVien(String strTieuChiTimKiem)
+        {
+            List<NhanVienDTO> lstNV =NhanVienDAO.DocDanhSachNV(strTieuChiTimKiem);
+            return lstNV;
+        }     
+
+            
+    }
+}
---
+++ trunk/SourceCode/TapHopModule/QuanLyNhanVien_TuanAnh/XoaNhanVienGUI.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BUS;
+using DTO;
+namespace GUI
+{
+    public partial class XoaNhanVienGUI : Form
+    {
+        public XoaNhanVienGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void XoaNhanVienGUI_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                List<NhanVienDTO> lstNV = NhanVienBUS.DocDanhSachNhanVien(String.Empty);
+                XuatDanhSachNV(lstNV);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+        private void XuatDanhSachNV(List<NhanVienDTO> lstNV)
+        {
+            if (lstNV == null)
+                return;     //khong lam gi ca.
+            
+            int nSoThuTu = 1;
+            lsv_DanhSachNhanVien.Items.Clear();
+          
+            foreach (NhanVienDTO aNV in lstNV)
+            {
+                lsv_DanhSachNhanVien.Items.Add(TheHienListItem(nSoThuTu,aNV));
+                
+                nSoThuTu++;
+            }
+        }
+
+        private ListViewItem TheHienListItem(int nSoThuTu, NhanVienDTO aNV)
+        {
+            ListViewItem itemKetQua = new ListViewItem(nSoThuTu.ToString());
+            itemKetQua.SubItems.Add(aNV.TenNhanVien);
+            itemKetQua.SubItems.Add(aNV.SoDienThoai.ToString());
+            itemKetQua.Tag = aNV;
+
+            return itemKetQua;
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            if(lsv_DanhSachNhanVien.CheckedItems.Count >0)
+            
+            {
+                try
+                {
+                    foreach (ListViewItem itemNV in lsv_DanhSachNhanVien.CheckedItems)
+                    {
+                        NhanVienDTO NV = (NhanVienDTO)itemNV.Tag;
+                       
+                        if (NhanVienBUS.XoaNhanVien(NV))
+                        {
+                            lsv_DanhSachNhanVien.Items.Remove(itemNV);
+                            
+
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Exception: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Chua co hang xe nao duoc chon");
+            }
+
+        }
+
+        private void lsv_DanhSachNhanVien_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lsv_DanhSachNhanVien.SelectedItems.Count>0)
+         
+            {
+                ListViewItem itemNV = lsv_DanhSachNhanVien.SelectedItems[0];
+                NhanVienDTO aNV = (NhanVienDTO)itemNV.Tag;
+                
+            }
+        }
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+       
+       
+       
+     
+
+
+        
+
+     
+     
+      
+    }
+}
